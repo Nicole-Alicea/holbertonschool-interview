7448ce3ac199a198f4de75a879028a8e7d7807c8
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-09-15 10:22:39 -0400

    fixing Betty

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -15,17 +15,17 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    /* Allocates memory for the new node */
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+  /* Allocates memory for the new node */
+  binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    if (!new_node)
-        return (NULL);  /* Returns NULL if memory allocation fails */
+  if (!new_node)
+    return (NULL);  /* Returns NULL if memory allocation fails */
 
-    /* Sets the value and pointers for the new node */
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+  /* Sets the value and pointers for the new node */
+  new_node->n = value;
+  new_node->parent = parent;
+  new_node->left = NULL;
+  new_node->right = NULL;
 
-    return (new_node);  /* Returns the newly created node */
+  return (new_node);  /* Returns the newly created node */
 }
