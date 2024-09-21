983c5bea582112b966986d8269fe2926468efebf
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-09-21 08:38:59 -0400

    task 0

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" minOperations """
+
+
+def minOperations(n):
+    """
+    Calculate the minimum number of operations required to reach exactly 'n' 
+    'H' characters starting with a single 'H'.
+
+    Args:
+        n (int): The target number of 'H' characters to reach.
+
+    Returns:
+        int: The minimum number of operations required. If `n` is less than or 
+        equal to 1 or if `n` is not an integer, return 0.
+    
+    The function calculates the minimum operations by breaking `n` down into 
+    prime factors. For each prime factor `p`, `p` operations are needed (copy 
+    and paste operations). The process continues until `n` is reduced to 1.
+    """
+    p = 2
+    num = 0
+
+    if type(n) != int or n <= 1:
+        return 0
+
+    while n != 1:
+        if n % p == 0:
+            n = n / p
+            num = num + p
+        else:
+            p = p + 1
+
+    return int(num)
