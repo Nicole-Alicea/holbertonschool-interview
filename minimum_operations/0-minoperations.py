4aa9019e118ba6bf9a4c1cb588a80e8defb93a68
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-09-21 08:41:24 -0400

    eliminating whitespaces

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,24 +4,24 @@
 
 def minOperations(n):
     """
-    Calculate the minimum number of operations required to reach exactly 'n' 
+    Calculate the minimum number of operations required to reach exactly 'n'
     'H' characters starting with a single 'H'.
 
     Args:
         n (int): The target number of 'H' characters to reach.
 
     Returns:
-        int: The minimum number of operations required. If `n` is less than or 
+        int: The minimum number of operations required. If `n` is less than or
         equal to 1 or if `n` is not an integer, return 0.
-    
-    The function calculates the minimum operations by breaking `n` down into 
-    prime factors. For each prime factor `p`, `p` operations are needed (copy 
+
+    The function calculates the minimum operations by breaking `n` down into
+    prime factors. For each prime factor `p`, `p` operations are needed (copy
     and paste operations). The process continues until `n` is reduced to 1.
     """
     p = 2
     num = 0
 
-    if type(n) != int or n <= 1:
+    if type(n) is not int or n <= 1:
         return 0
 
     while n != 1:
