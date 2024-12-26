1b7b6734f00a79254c88c2c648b6b01b8066f106
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-12-26 14:09:16 -0500

    adding print_array.c

--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -5,26 +5,6 @@
 #include <stdlib.h>
 
 void heap_sort(int *array, size_t size);
-
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
+void print_array(const int *array, size_t size);
 
 #endif /* SORT_H */
