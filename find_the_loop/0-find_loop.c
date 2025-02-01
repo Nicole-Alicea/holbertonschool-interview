c9eba1c81a8f0c29f6f40c93ea48ccb7f08da69c
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2025-02-01 08:21:28 -0500

    fixing tabs - Betty

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -2,23 +2,23 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow = head, *fast = head;
-    
-    while (fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-        
-        if (slow == fast) /* Loop detected */
-        {
-            slow = head;
-            while (slow != fast)
-            {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return slow; /* Start of the loop */
-        }
-    }
-    return NULL; /* No loop */
+	listint_t *slow = head, *fast = head;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast) /* Loop detected */
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return slow; /* Start of the loop */
+		}
+	}
+	return NULL; /* No loop */
 }
